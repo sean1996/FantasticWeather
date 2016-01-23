8b53f81eda53bb4100ace6e18b0b5c039b1c0de8
Xinghan Wang <xinghanw@usc.edu>
2016-01-23 15:37:59 -0800

    Change Status bar to light

--- UltraWeather/AppDelegate.swift
+++ UltraWeather/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         GMSServices.provideAPIKey("AIzaSyA_X9Ds2IYkb9if9MfTnvwa4PKsUrBGi3c")
+        UIApplication.sharedApplication().statusBarStyle = .LightContent
         return true
     }
 
